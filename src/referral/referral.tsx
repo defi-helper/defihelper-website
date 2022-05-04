c7084f2430dae103a0ae65cc61b68ea15b0d2148
Luka Nikitin <lukentuichennel@gmail.com>
2022-05-04 16:46:14 +0200

    fix

--- src/referral/referral.tsx
+++ src/referral/referral.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { useUserReferrerQuery } from 'src/graphql/_generated-hooks';
 import Cookies from 'js-cookie';
@@ -11,16 +11,18 @@ export const Referral: React.VFC = () => {
     }
   });
 
-  if (!data) {
-    window.location.href = 'https://app.defihelper.io';
-    return <>Wait...</>;
-  }
+  useEffect(() => {
+    if (!data) {
+      window.location.href = 'https://app.defihelper.io';
+      return;
+    }
 
-  Cookies.set('dfh-parent-code', data.userReferrer.id, {
-    expires: 1000 * 60 * 60 * 24 * 365, // 1 year
-    domain: 'defihelper.io'
-  });
-  window.location.href = data.userReferrer.redirectTo;
+    Cookies.set('dfh-parent-code', data.userReferrer.id, {
+      expires: 1000 * 60 * 60 * 24 * 365, // 1 year
+      domain: 'defihelper.io'
+    });
+    window.location.href = data.userReferrer.redirectTo;
+  }, [data]);
 
   return <div style={{ textAlign: 'center', padding: 15 }}>Wait...</div>;
 };
